7eb5f261398dc19179bb731b322b54d588ec8cbc
Roben Duncan Kipkoech <roben.duncan@student.moringaschool.com>
2026-09-25 17:22:51 +0300

    I updated the functions to manipulate the array

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,38 @@
-// Write your code here
+// Array
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+//Function to log first product
+function logFirstProduct(arr){
+  console.log(arr[0]);
+}
+logFirstProduct(products);
+//Function to add product
+function addProduct(newProduct){
+  products.push(newProduct);
+}
+addProduct("Tablet");
+console.log(products);
+
+//Function to update product name
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  } else {
+    console.log("Invalid index position");
+
+  }
+}
+updateProductName(1, "Computer");
+console.log(products);
+
+//Function to delete a product
+function removeLastProduct(){
+  products.pop();
+}
+removeLastProduct();
+console.log(products);
+
+
+
 
 
 
